a65f23266b66cee3726f541879cc353fd46cebf5
William Thompson <will.thompson@outlook.com>
2022-02-14 08:57:39 -0800

    first commit

---
+++ pythontest1.py
@@ -0,0 +1,11 @@
+
+import time
+start = time.time()
+for i in range(0,1_000_000):
+    i * i
+end = time.time()
+elapsed = (end - start)
+
+instrs = 3.5e9 * elapsed/1000000
+
+print(f"Est. instructions per Python multiply: {instrs}")
